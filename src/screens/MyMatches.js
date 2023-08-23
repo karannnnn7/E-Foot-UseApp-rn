60971fd71b6774cd8d1723a785a5cc7619fdbdd9
Karannn <karannnofficial7@gmail.com>
2023-08-23 15:30:53 +0530

    Fixing bug and change popup open

--- src/screens/MyMatches.js
+++ src/screens/MyMatches.js
@@ -1,13 +1,10 @@
 import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, Modal, Dimensions, Animated, ScrollView } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
+import { View, Text, TouchableOpacity, Dimensions, ScrollView } from 'react-native';
 import { TextInput } from 'react-native-paper';
-import * as DocumentPicker from 'expo-document-picker';
 import CommonHeader from '../components/CommonHeader';
 import MyMatchesHeaderSvg from '../../assets/svg/MyMatchesHeader.svg';
 import SearchSvg from '../../assets/svg/Search.svg';
 import FilterSvg from '../../assets/svg/Filter.svg';
-import CloseSvg from '../../assets/svg/Close.svg';
 import PendingTab from './RenderTabs/PendingTab';
 import AcceptedTab from './RenderTabs/AcceptedTab';
 import DisputeTab from './RenderTabs/DisputeTab';
@@ -18,36 +15,11 @@ import DrawTab from './RenderTabs/DrawTab';
 const MyMatches = ({ navigation }) => {
 
     const isSmallScreen = Dimensions.get('screen').height > 850;
-    const [visible, setVisible] = useState(false);
-    const [animationValue, setanimationValue] = useState(new Animated.Value(1));
-    const [isDocumentUploaded, setIsDocumentUploaded] = useState(false);
     const [selected, setSelected] = useState('Pending (10)');
     const [activeTabName, setActiveTabName] = useState('Pending (10)');
 
 
-    // For modal
-    const showModal = () => {
-        setVisible(true);
-        Animated.timing(animationValue, {
-            toValue: 0,
-            duration: 500,
-            useNativeDriver: true,
-        }).start();
-    };
-    const hideModal = () => {
-        setVisible(false);
-        setanimationValue(new Animated.Value(1));
-    };
 
-    //For Document Pick form phonestorage
-    const handlePickDocument = async () => {
-        let result = await DocumentPicker.getDocumentAsync({});
-        if (result.type === 'success') {
-            setIsDocumentUploaded(true); // Set the state to indicate document upload
-        }
-        alert(result.uri);
-        console.log(result);
-    };
 
     //For Changing Tab Content
     const renderContent = () => {
@@ -103,80 +75,11 @@ const MyMatches = ({ navigation }) => {
                                 </View>
                             </View>
 
-                            <TouchableOpacity onPress={() => setVisible(true)} className="bg-[#261D37] p-4 rounded-lg mt-1">
+                            <TouchableOpacity onPress={() => console.log('pressed')} className="bg-[#261D37] p-4 rounded-lg mt-1">
                                 <FilterSvg />
                             </TouchableOpacity>
 
-                            <Modal
-                                // animationType="slide"
-                                transparent={true}
-                                visible={visible}
-                            // onRequestClose={() => {
-                            //   setVisible(!visible);
-                            // }}
-                            >
-
-                                <TouchableOpacity onPress={hideModal}>
-                                    <View className="h-full w-full bg-[#000000bf]" />
-                                </TouchableOpacity>
-
-                                <View className="absolute bottom-0 bg-[#261D37] w-full rounded-t-2xl" >
-                                    <View className="p-3 mt-3">
-
-                                        <View className="flex-row items-center justify-between mx-5 space-x-3 my-5">
-                                            <Text className="font-ChakraPetchBold text-lg text-[#D1CBD8]">Proof Submission</Text>
-                                            <TouchableOpacity onPress={() => hideModal()}>
-                                                <CloseSvg />
-                                            </TouchableOpacity>
-                                        </View>
-
-                                        <View className="items-center">
-                                            <TouchableOpacity onPress={handlePickDocument} className='h-12 items-center justify-center w-[350px] mt-7'>
-                                                <LinearGradient
-                                                    colors={['#4A00E8', '#3B3EFF']}
-                                                    className="h-full absolute top-0 bottom-0 left-0 right-0 rounded-lg"
-                                                />
-                                                <Text className="font-ChakraPetchBold text-sm text-[#FFFFFF]">Upload Proof</Text>
-                                            </TouchableOpacity>
-                                        </View>
-
-                                        <View className="bg-[#332d1c] mt-5 mx-5 p-2 rounded-lg">
-                                            <Text className="font-ChakraPetchBold text-sm text-[#FFC107]">Note:</Text>
-                                            <Text className="font-ChakraPetchLight text-sm text-[#FFC107]">Upload screenshot or score image of game to resolve dispute. Make sure scores are visible properly Uploaded image will be used for inspecting scores. Once uploaded you can't reupload it again</Text>
-                                        </View>
-
-                                        {isSmallScreen ? (<View className="my-10 mx-5 flex-row items-center justify-between">
-                                            <TouchableOpacity onPress={() => hideModal(true)} className=" items-center border border-[#D1CBD8] p-3 rounded-lg w-40">
-                                                <Text className="font-ChakraPetchBold text-base text-[#D1CBD8]">Cancel</Text>
-                                            </TouchableOpacity>
-
-                                            {isDocumentUploaded ? (<TouchableOpacity onPress={() => console.log('pressed')} className='h-[52px] items-center justify-center w-40'>
-                                                <LinearGradient
-                                                    colors={['#4A00E8', '#3B3EFF']}
-                                                    className="h-full absolute top-0 bottom-0 left-0 right-0 rounded-lg"
-                                                />
-                                                <Text className="font-ChakraPetchBold text-sm text-[#FFFFFF]">Submit</Text>
-                                            </TouchableOpacity>) : (<TouchableOpacity onPress={() => console.log('pressed')} className='h-[52px] items-center justify-center w-40 bg-[#98959A] rounded-lg'>
-                                                <Text className="font-ChakraPetchBold text-sm text-[#FFFFFF]">Submit</Text>
-                                            </TouchableOpacity>)}
-                                        </View>) : (<View className="my-10 mx-5 flex-row items-center justify-between space-x-5">
-                                            <TouchableOpacity onPress={() => hideModal(true)} className=" items-center border border-[#D1CBD8] p-3 rounded-lg w-40">
-                                                <Text className="font-ChakraPetchBold text-base text-[#D1CBD8]">Cancel</Text>
-                                            </TouchableOpacity>
-
-                                            {isDocumentUploaded ? (<TouchableOpacity onPress={() => console.log('pressed')} className='h-[52px] items-center justify-center w-40'>
-                                                <LinearGradient
-                                                    colors={['#4A00E8', '#3B3EFF']}
-                                                    className="h-full absolute top-0 bottom-0 left-0 right-0 rounded-lg"
-                                                />
-                                                <Text className="font-ChakraPetchBold text-sm text-[#FFFFFF]">Apply</Text>
-                                            </TouchableOpacity>) : (<TouchableOpacity onPress={() => console.log('pressed')} className='h-[52px] items-center justify-center w-40 bg-[#98959A] rounded-lg'>
-                                                <Text className="font-ChakraPetchBold text-sm text-[#FFFFFF]">Apply</Text>
-                                            </TouchableOpacity>)}
-                                        </View>)}
-                                    </View>
-                                </View>
-                            </Modal>
+                            
                         </View>
                     </View>
 
--- src/screens/RenderTabs/DisputeTab.js
+++ src/screens/RenderTabs/DisputeTab.js
@@ -1,10 +1,42 @@
-import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, Modal, Dimensions, Animated } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
+import * as DocumentPicker from 'expo-document-picker';
 import SmallLogoSvg from '../../../assets/svg/SmallLogo.svg';
 import PPSvg from '../../../assets/svg/PP.svg';
+import CloseSvg from '../../../assets/svg/Close.svg';
 
 const DisputeTab = () => {
+
+  const isSmallScreen = Dimensions.get('screen').height > 850;
+  const [visible, setVisible] = useState(false);
+  const [animationValue, setanimationValue] = useState(new Animated.Value(1));
+  const [isDocumentUploaded, setIsDocumentUploaded] = useState(false);
+
+  // For modal
+  const showModal = () => {
+    setVisible(true);
+    Animated.timing(animationValue, {
+      toValue: 0,
+      duration: 500,
+      useNativeDriver: true,
+    }).start();
+  };
+  const hideModal = () => {
+    setVisible(false);
+    setanimationValue(new Animated.Value(1));
+  };
+
+  //For Document Pick form phonestorage
+  const handlePickDocument = async () => {
+    let result = await DocumentPicker.getDocumentAsync({});
+    if (result.type === 'success') {
+      setIsDocumentUploaded(true); // Set the state to indicate document upload
+    }
+    alert(result.uri);
+    console.log(result);
+  };
+
   return (
     <>
       <View>
@@ -62,7 +94,7 @@ const DisputeTab = () => {
           </View>
 
           <View className="items-center">
-            <TouchableOpacity onPress={() => console.log('pressed')} className='h-12 items-center justify-center w-[310px] mt-7'>
+            <TouchableOpacity onPress={() => setVisible(true)} className='h-12 items-center justify-center w-[310px] mt-7'>
               <LinearGradient
                 colors={['#4A00E8', '#3B3EFF']}
                 className="h-full absolute top-0 bottom-0 left-0 right-0 rounded-lg"
@@ -70,6 +102,77 @@ const DisputeTab = () => {
               <Text className="font-ChakraPetchBold text-sm text-[#FFFFFF]">Resolve Dispute</Text>
             </TouchableOpacity>
           </View>
+
+          <Modal
+            // animationType="slide"
+            transparent={true}
+            visible={visible}
+          // onRequestClose={() => {
+          //   setVisible(!visible);
+          // }}
+          >
+
+            <TouchableOpacity onPress={hideModal}>
+              <View className="h-full w-full bg-[#000000bf]" />
+            </TouchableOpacity>
+
+            <View className="absolute bottom-0 bg-[#261D37] w-full rounded-t-2xl" >
+              <View className="p-3 mt-3">
+
+                <View className="flex-row items-center justify-between mx-5 space-x-3 my-5">
+                  <Text className="font-ChakraPetchBold text-lg text-[#D1CBD8]">Proof Submission</Text>
+                  <TouchableOpacity onPress={() => hideModal()}>
+                    <CloseSvg />
+                  </TouchableOpacity>
+                </View>
+
+                <View className="items-center">
+                  <TouchableOpacity onPress={handlePickDocument} className='h-12 items-center justify-center w-[350px] mt-7'>
+                    <LinearGradient
+                      colors={['#4A00E8', '#3B3EFF']}
+                      className="h-full absolute top-0 bottom-0 left-0 right-0 rounded-lg"
+                    />
+                    <Text className="font-ChakraPetchBold text-sm text-[#FFFFFF]">Upload Proof</Text>
+                  </TouchableOpacity>
+                </View>
+
+                <View className="bg-[#332d1c] mt-5 mx-5 p-2 rounded-lg">
+                  <Text className="font-ChakraPetchBold text-sm text-[#FFC107]">Note:</Text>
+                  <Text className="font-ChakraPetchLight text-sm text-[#FFC107]">Upload screenshot or score image of game to resolve dispute. Make sure scores are visible properly Uploaded image will be used for inspecting scores. Once uploaded you can't reupload it again</Text>
+                </View>
+
+                {isSmallScreen ? (<View className="my-10 mx-5 flex-row items-center justify-between">
+                  <TouchableOpacity onPress={() => hideModal(true)} className=" items-center border border-[#D1CBD8] p-3 rounded-lg w-40">
+                    <Text className="font-ChakraPetchBold text-base text-[#D1CBD8]">Cancel</Text>
+                  </TouchableOpacity>
+
+                  {isDocumentUploaded ? (<TouchableOpacity onPress={() => console.log('pressed')} className='h-[52px] items-center justify-center w-40'>
+                    <LinearGradient
+                      colors={['#4A00E8', '#3B3EFF']}
+                      className="h-full absolute top-0 bottom-0 left-0 right-0 rounded-lg"
+                    />
+                    <Text className="font-ChakraPetchBold text-sm text-[#FFFFFF]">Submit</Text>
+                  </TouchableOpacity>) : (<TouchableOpacity onPress={() => console.log('pressed')} className='h-[52px] items-center justify-center w-40 bg-[#98959A] rounded-lg'>
+                    <Text className="font-ChakraPetchBold text-sm text-[#FFFFFF]">Submit</Text>
+                  </TouchableOpacity>)}
+                </View>) : (<View className="my-10 mx-5 flex-row items-center justify-between space-x-5">
+                  <TouchableOpacity onPress={() => hideModal(true)} className=" items-center border border-[#D1CBD8] p-3 rounded-lg w-40">
+                    <Text className="font-ChakraPetchBold text-base text-[#D1CBD8]">Cancel</Text>
+                  </TouchableOpacity>
+
+                  {isDocumentUploaded ? (<TouchableOpacity onPress={() => console.log('pressed')} className='h-[52px] items-center justify-center w-40'>
+                    <LinearGradient
+                      colors={['#4A00E8', '#3B3EFF']}
+                      className="h-full absolute top-0 bottom-0 left-0 right-0 rounded-lg"
+                    />
+                    <Text className="font-ChakraPetchBold text-sm text-[#FFFFFF]">Apply</Text>
+                  </TouchableOpacity>) : (<TouchableOpacity onPress={() => console.log('pressed')} className='h-[52px] items-center justify-center w-40 bg-[#98959A] rounded-lg'>
+                    <Text className="font-ChakraPetchBold text-sm text-[#FFFFFF]">Apply</Text>
+                  </TouchableOpacity>)}
+                </View>)}
+              </View>
+            </View>
+          </Modal>
         </View>
 
         <View className="bg-[#261D37] p-4 rounded-md mt-5">
@@ -126,7 +229,7 @@ const DisputeTab = () => {
           </View>
 
           <View className="items-center">
-            <TouchableOpacity onPress={() => console.log('pressed')} className='h-12 items-center justify-center w-[310px] mt-7'>
+            <TouchableOpacity onPress={() => setVisible(true)} className='h-12 items-center justify-center w-[310px] mt-7'>
               <LinearGradient
                 colors={['#4A00E8', '#3B3EFF']}
                 className="h-full absolute top-0 bottom-0 left-0 right-0 rounded-lg"
