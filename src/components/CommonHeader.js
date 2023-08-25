daa0ffd5fc5cb9db8f4433b0e1bfcb4baa6d7324
Karannn <karannnofficial7@gmail.com>
2023-08-25 15:33:49 +0530

    fixing bugs

--- src/components/CommonHeader.js
+++ src/components/CommonHeader.js
@@ -22,9 +22,9 @@ const CommonHeader = ({ navigation }) => {
                 </TouchableOpacity>
 
                 <View className="flex-row items-center space-x-5">
-                    <TouchableOpacity className="bg-transparent bg-[#2c2544] border border-[#3B3EFF] rounded-xl p-3">
+                    <View className="bg-transparent bg-[#2c2544] border border-[#3B3EFF] rounded-xl p-3">
                         <Text className="text-white text-sm font-ChakraPetchBold">0 - 5 WINS</Text>
-                    </TouchableOpacity>
+                    </View>
 
                     <TouchableOpacity>
                         <NotificationSvg />
